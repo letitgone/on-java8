3f0b076c6395e595d963ba3c2266f58ade08987f
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2020-04-20 08:13:04 +0800

    ZhangGJ:Object assignment

---
+++ chapter04/src/main/java/operators/Equivalence.java
@@ -0,0 +1,14 @@
+package operators;
+
+/**
+ * @Author ZhangGJ
+ * @Date 2020/04/04 08:11
+ */
+public class Equivalence {
+    public static void main(String[] args) {
+        Integer n1 = 47;
+        Integer n2 = 47;
+        System.out.println(n1 == n2);
+        System.out.println(n1 != n2);
+    }
+}
---
+++ chapter04/src/main/java/operators/PassObject.java
@@ -0,0 +1,23 @@
+package operators;
+
+class Letter {
+    char c;
+}
+
+/**
+ * @Author ZhangGJ
+ * @Date 2020/04/04 07:58
+ */
+public class PassObject {
+    static void f(Letter y) {
+        y.c = 'z';
+    }
+
+    public static void main(String[] args) {
+        Letter x = new Letter();
+        x.c = 'a';
+        System.out.println("1: x.c: " + x.c);
+        f(x);
+        System.out.println("2: x.c: " + x.c);
+    }
+}
